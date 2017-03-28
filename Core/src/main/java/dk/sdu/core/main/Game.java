8d7700a0c7a394d98a785fb4e01d290c0282bc68
Fatih Ozcelik <faozc14@student.sdu.dk>
2017-03-28 14:26:39 +0200

    first commit

--- Core/src/main/java/dk/sdu/core/main/Game.java
+++ Core/src/main/java/dk/sdu/core/main/Game.java
@@ -5,10 +5,57 @@
  */
 package dk.sdu.core.main;
 
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import org.openide.util.Lookup;
+
 /**
  *
  * @author fatihozcelik
  */
-public class Game {
+public class Game implements ApplicationListener {
     
+    private static OrthographicCamera cam;
+    private ShapeRenderer sr;
+    private final Lookup lookup = Lookup.getDefault();
+       
+
+    @Override
+    public void create() {
+        cam = new OrthographicCamera(800, 600);
+        cam.translate(800/2, 600/2);
+        cam.update();
+        
+        sr = new ShapeRenderer();
+        
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+    }
+
+    @Override
+    public void render() {
+        // clear screen to black
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);   
+        
+        
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+
 }
