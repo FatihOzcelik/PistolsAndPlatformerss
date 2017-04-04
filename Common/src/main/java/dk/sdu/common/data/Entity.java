0323d16c674ca3fb10b046a2124bf771b84bd3de
Arian <Arian@AriansComputer>
2017-04-04 11:02:43 +0200

    Lavet variablerne i entity

--- Common/src/main/java/dk/sdu/common/data/Entity.java
+++ Common/src/main/java/dk/sdu/common/data/Entity.java
@@ -11,4 +11,79 @@ package dk.sdu.common.data;
  */
 public class Entity {
     
+    float positionX;
+    float positionY;
+    float maxSpeed;
+    float acceleration;
+    float deacceleration;
+    boolean direction;
+    int health;
+
+    public Entity(float positionX, float positionY, float maxSpeed, float acceleration, float deacceleration, boolean direction, int health) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.maxSpeed = maxSpeed;
+        this.acceleration = acceleration;
+        this.deacceleration = deacceleration;
+        this.direction = direction;
+        this.health = health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setPositionX(float positionX) {
+        this.positionX = positionX;
+    }
+
+    public void setPositionY(float positionY) {
+        this.positionY = positionY;
+    }
+
+    public void setMaxSpeed(float maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void setAcceleration(float acceleration) {
+        this.acceleration = acceleration;
+    }
+
+    public void setDeacceleration(float deacceleration) {
+        this.deacceleration = deacceleration;
+    }
+
+    public void setDirection(boolean direction) {
+        this.direction = direction;
+    }
+
+    public float getPositionX() {
+        return positionX;
+    }
+
+    public float getPositionY() {
+        return positionY;
+    }
+
+    public float getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public float getAcceleration() {
+        return acceleration;
+    }
+
+    public float getDeacceleration() {
+        return deacceleration;
+    }
+
+    public boolean isDirection() {
+        return direction;
+    }
+    
+    
 }
