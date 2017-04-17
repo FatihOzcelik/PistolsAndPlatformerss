81f287c949cd377a6d443621f588b319bc9ee5a8
Frank Sebastian <frseb15@student.sdu.dk>
2017-04-17 15:10:42 +0200

    Opsætning af Enemy (indeholder fejl)

--- Player/src/main/java/dk/sdu/player/EntityPlugin.java
+++ Player/src/main/java/dk/sdu/player/EntityPlugin.java
@@ -17,12 +17,22 @@ public class EntityPlugin implements IPluginService {
 
     @Override
     public void start(GameData gameData, World world) {
+        
+        Enemy = new Entity (10, 100, 100, 20, 2, true, 100);
+        Enemy.setSprite("C:\\Users\\Frank Sebastian\\Documents\\NetBeansProjects\\PistolsAndPlatformerss\\Core\\target\\classes\\dk\\sdu\\core\\assets\\enemy.png");
+    
+        world.addEntity(Enemy);
+        
+        
         player = new Entity(10, 100, 100, 20, 2, true, 100);
 //      player.setSprite("/Users/fatihozcelik/NetBeansProjects/PistolsAndPlatformerss/Core/src/main/resources/dk/sdu/core/assets/player.png");
 //      player.setSprite("/Users/Arian/Desktop/skole/Objekt/code/PistolsAndPlatformerss/Core/target/classes/dk/sdu/core/assets/player.png");
         
-        player.setSprite("/Users/fatihozcelik/NetBeansProjects/PistolsAndPlatformerss/Core/src/main/resources/dk/sdu/core/assets/player.png");
+        player.setSprite("\\Users\\Frank Sebastian\\Documents\\NetBeansProjects\\PistolsAndPlatformerss\\Core\\target\\classes\\dk\\sdu\\core\\assets\\player.png");
+      //  player.setSprite("/Users/fatihozcelik/NetBeansProjects/PistolsAndPlatformerss/Core/src/main/resources/dk/sdu/core/assets/player.png");
         world.addEntity(player);
+    
+        
     }
 
     @Override
