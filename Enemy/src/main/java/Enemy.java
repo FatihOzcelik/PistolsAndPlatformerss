e6214148b2cdce1483435e67532ba424065a2901
Frank Sebastian <frseb15@student.sdu.dk>
2017-04-17 15:10:22 +0200

    Opsætning af Enemy (indeholder fejl)

---
+++ Enemy/src/main/java/Enemy.java
@@ -0,0 +1,41 @@
+
+import dk.sdu.common.data.Entity;
+import dk.sdu.common.data.GameData;
+import dk.sdu.common.data.World;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Frank Sebastian
+ */
+public class Enemy  {
+    
+    private float positionX, positionY;
+    
+    public Enemy(float positionX, float positionY){
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+    public void update(){
+        
+    }
+    
+    public void draw(Graphics2D g2d){
+        g2d.drawImage(getEnemyImg, positionX, positionY);
+        
+    }
+    
+    public Image getEnemyImg(){
+       ImageIcon ic = new ImageIcon("\\Users\\Frank Sebastian\\Documents\\NetBeansProjects\\PistolsAndPlatformerss\\Core\\target\\classes\\dk\\sdu\\core\\assets\\enemy.png");
+       return ic.getImage();
+    }
+     
+}
